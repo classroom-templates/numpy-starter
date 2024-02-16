991c6f511b506f8d9c8eb7e6829da0c767b86d89
Alexander Katrompas <alexander.katrompas@austincc.edu>
2024-02-16 16:02:17 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,57 @@
+##########################
+# 
+# Your comment header here
+# 
+##########################
+
+# the libraries needed, do not alter these
+import random as rnd
+import numpy as np
+
+# start with these two control constants to get your code working and then,
+# alter them to prove your code works,change to any positive numbers from 1 to n,
+# where n can be any number. put them back to 4 and 5 when you are done testing
+# and ready to submit.
+ROWS = 5
+COLS = 4
+
+# start with this control constant to get your code working and then change
+# it to any number 1 to n, where n can be any number, to prove your code works.
+# put it back to 100 when you are done testing and ready to submit.
+RANDOM_LIMIT = 100
+
+def main():
+
+    # create a 2D numpy array of ints of size ROWS x COLS filled with zeros using np.zeros
+
+
+
+    # print the array of zeros here as show in the examples
+
+
+
+
+    # fill the array with random numbers 1 to RANDOM_LIMIT
+
+
+
+
+    # print the array here as show in the examples
+
+
+
+
+    # print the whole array sum and average as shown in the examples
+
+
+
+
+    # print the array sum and averages by row as shown in the examples
+
+
+
+
+    # print the array mins and maxs as shown in the examples
+
+
+main()
